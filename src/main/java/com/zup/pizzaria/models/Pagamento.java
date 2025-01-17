c0950833d1bca584df80983ddde5762abc34b0de
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-17 10:59:54 -0300

    feat: Criação das enumerações FormaPagamento

--- src/main/java/com/zup/pizzaria/models/Pagamento.java
+++ src/main/java/com/zup/pizzaria/models/Pagamento.java
@@ -1,9 +1,10 @@
 package com.zup.pizzaria.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.NotFound;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Pagamento {
@@ -13,6 +14,19 @@ public class Pagamento {
 
     @NotNull(message = "O ID do pedido não pode ser nulo.")
     private Long pedidoId;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "A forma de pagamento não pode ser nula.")
+    private FormaPagamento formaPagamento;
+
+    @NotNull(message = "O valor pago não pode ser nulo.")
+    @min(value = 0, message = "O valor pago deve ser maior ou igual a zero.")
+    private Double valorPago;
+
+    @NotNull(message = "A data e hora do pagamento não podem ser nulas.")
+    private LocalDateTime dataHoraPagamento;
+
+
 
 
 }
