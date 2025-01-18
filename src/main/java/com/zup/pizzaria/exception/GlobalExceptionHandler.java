2279778bf245928c7ff6070e86a255f269919968
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-18 10:03:46 -0300

    feat: Implementando a classe GlobalExceptionHandler para tratamento de erros Globais

---
+++ src/main/java/com/zup/pizzaria/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.zup.pizzaria.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentConversionNotSupportedException;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentConversionNotSupportedException.class)
+    public ResponseEntity<Map<String, String>> handValidationExceptions(MethodArgumentConversionNotSupportedException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+}
